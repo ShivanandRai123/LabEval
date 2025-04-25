f53dcb3d48499c5473144031bafc6f95de59bd7d
Shivanand Rai <shivanandrai24@gmail.com>
2025-04-25 07:08:13 +0530

    Initial commit: Added square.py to calculate square of 5

---
+++ square.py
@@ -0,0 +1,8 @@
+# square.py
+def calculate_square():
+    number = 5
+    result = number ** 2
+    print(f"The square of {number} is {result}")
+
+if __name__ == "__main__":
+    calculate_square()
